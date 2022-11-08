7625a5d4fd8bf353ea4f2484753e189d2acb99dc
Dhananjay Mishra <dhananjay.mishra@minditsystems.com>
2022-11-08 11:09:34 +0530

    update

---
+++ National-ERP/National.Services/IServices/IUserServices.cs
@@ -0,0 +1,19 @@
+﻿
+using Nation.Services.ServiceResponse;
+using National.Domain.Logic.DTO;
+using National.Services.ServiceRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace National.Services.IServices
+{
+    public interface IUserServices
+    {
+        Task<UserDto?> GetUserRecords(int userId);
+         Task<UserResponse> Authenticate(UserRequest user);
+        Task<string> RegisterUser(UserRequest user); 
+    }
+}
---
+++ National-ERP/National.Services/ServiceResponse/UserResponse.cs
@@ -0,0 +1,20 @@
+﻿using National.Domain.Logic.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nation.Services.ServiceResponse
+{
+    public class UserResponse
+    {
+      public  UserResponse()
+        {
+            UserInfo = new UserDto();
+        }
+        public UserDto? UserInfo{get;set;}
+        public string? accessToken { get; set; }
+        public string? Message { get; set; }
+    }
+}
